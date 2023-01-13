19117988e4444c9629d5d8a0d96c5232502be5ac
luoxiaojun <xiao-jun.luo@hp.com>
2023-01-13 16:32:34 +0800

    refactor: do code refactor

--- controllers/pod_controller.go
+++ controllers/pod_controller.go
@@ -19,13 +19,13 @@ package controllers
 import (
 	"context"
 	"fmt"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -62,69 +62,26 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	}
 
 	metricConfig := instance.Spec.Metric
+	appName := instance.Name + "." + metricConfig
 
-	// Deploy monitor components
-	if metricConfig == "grafana" {
-		appName := instance.Name + "." + metricConfig
+	// Check whether the monitor component exists
+	monitorExisted, err := r.monitorComponentExisted(ctx, appName, instance.Namespace)
+	if err != nil {
+		logger.Error(err, fmt.Sprintf("Error while checking existed monitor: %s", err))
+		return ctrl.Result{}, err
+	}
 
-		// Check whether the monitor component exists
-		monitorExisted := true
-		existedMonitor := &appsv1.Deployment{}
-		if err := r.Get(ctx, types.NamespacedName{
-			Name:      appName,
-			Namespace: instance.Namespace,
-		}, existedMonitor); err != nil {
-			if errors.IsNotFound(err) {
-				monitorExisted = false
-			} else {
-				logger.Error(err, fmt.Sprintf("Error while getting existed monitor: %s", err))
-				return ctrl.Result{}, err
-			}
+	// Create the monitor component if no monitor component
+	if !monitorExisted {
+		monitorDeploy := r.monitorComponentDeployment(appName, instance.Namespace, 1)
+		err = r.Client.Create(ctx, monitorDeploy)
+		if err != nil {
+			logger.Error(err, fmt.Sprintf("Error while creating monitor: %s", err))
+			return ctrl.Result{}, err
 		}
-
-		// Create the monitor component if no monitor component
-		if !monitorExisted {
-			var replicas int32 = 1
-			appLabels := map[string]string{"app": appName}
-			monitor := &appsv1.Deployment{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      appName,
-					Namespace: instance.Namespace,
-				},
-				Spec: appsv1.DeploymentSpec{
-					Replicas: &replicas,
-					Selector: &metav1.LabelSelector{
-						MatchLabels: appLabels,
-					},
-					Template: corev1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{
-							Labels: appLabels,
-						},
-						Spec: corev1.PodSpec{
-							Containers: []corev1.Container{
-								{
-									Name:  "grafana",
-									Image: "grafana/grafana",
-									Ports: []corev1.ContainerPort{
-										{
-											ContainerPort: 3000,
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			}
-			err = r.Client.Create(ctx, monitor)
-			if err != nil {
-				logger.Error(err, fmt.Sprintf("Error while creating monitor: %s", err))
-				return ctrl.Result{}, err
-			}
-			return ctrl.Result{
-				Requeue: true,
-			}, nil
-		}
+		return ctrl.Result{
+			Requeue: true,
+		}, nil
 	}
 
 	instance.Status.Result = "Successful"
@@ -143,4 +100,59 @@ func (r *PodReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&monitorv1alpha1.Pod{}).
 		Owns(&appsv1.Deployment{}).
 		Complete(r)
+}
+
+func (r *PodReconciler) monitorComponentExisted(ctx context.Context, appName, namespace string) (bool, error) {
+	if _, err := r.getMonitorComponentDeployment(ctx, appName, namespace); err != nil {
+		if errors.IsNotFound(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (r *PodReconciler) getMonitorComponentDeployment(ctx context.Context, appName, namespace string) (*appsv1.Deployment, error) {
+	existedMonitorDeploy := &appsv1.Deployment{}
+	if err := r.Get(ctx, types.NamespacedName{
+		Name:      appName,
+		Namespace: namespace,
+	}, existedMonitorDeploy); err != nil {
+		return nil, err
+	}
+	return existedMonitorDeploy, nil
+}
+
+func (r *PodReconciler) monitorComponentDeployment(appName, namespace string, replicas int32) *appsv1.Deployment {
+	appLabels := map[string]string{"app": appName}
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      appName,
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: appLabels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: appLabels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "grafana",
+							Image: "grafana/grafana",
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 3000,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 }
